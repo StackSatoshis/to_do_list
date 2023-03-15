959e1d526309198fe58ccfa850c331a384f69144
Ryan Carroll <software@ryancarroll.io>
2023-03-15 12:31:30 -0400

    optimized arguments

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 # Ryan Carroll
+
+def get_todos(filepath="todos.txt"):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath="todos.txt"):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
 while True:
     # Gets user input and strips white space
     user_action = input("Type add, show, edit, complete or exit:")
@@ -7,59 +18,50 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         todos.append(todo + '\n')
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
-
+        write_todos(todos)
 
     elif user_action.startswith("show"):
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
             row = f"{index + 1 } - {item}"
             print(row)
 
-
     elif user_action.startswith("edit"):
         try:
             number = int(user_action[5:])
             number = number - 1
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             new_todo = input("enter new todo: ")
             todos[number] = new_todo + "\n"
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos(todos)
+
         except ValueError:
             print("Invalid command.")
             continue
-
 
     elif user_action.startswith("complete"):
         try:
             number = int(user_action[9:])
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
-
+            todos = get_todos()
             index = number - 1
             to_be_removed = todos[index].strip('\n')
             todos.pop(index)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos(todos)
 
             message = f"{to_be_removed} was removed from the list."
             print(message)
+            
         except IndexError:
             print("No todo with that number. Please try again. ")
             continue
