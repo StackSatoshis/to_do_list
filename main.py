b11f57cde240c68dc6a35beeb300a70f5b7cece7
Ryan Carroll <software@ryancarroll.io>
2023-03-15 12:52:20 -0400

    optimized arguments

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 # Ryan Carroll
 
 def get_todos(filepath="todos.txt"):
+    '''
+    Reads a text file and returns
+    list of todo items
+    '''
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
 def write_todos(todos_arg, filepath="todos.txt"):
+    '''
+    Writes the to-do items to a file
+    :param todos_arg:
+    :param filepath:
+    :return:
+    '''
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
