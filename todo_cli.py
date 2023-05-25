5a1be6e509a518430c2db5723a27fa169c1777c5
Ryan Carroll <software@ryancarroll.io>
2023-05-25 09:43:22 -0400

    created basic gui file

--- todo_cli.py
+++ todo_cli.py
@@ -1,24 +1,9 @@
 # Ryan Carroll
+from functions import get_todos, write_todos
+import time
 
-def get_todos(filepath="todos.txt"):
-    '''
-    Reads a text file and returns
-    list of todo items
-    '''
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    '''
-    Writes the to-do items to a file
-    :param todos_arg:
-    :param filepath:
-    :return:
-    '''
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
 
 while True:
     # Gets user input and strips white space
